b6cbcfe5ff807215865344c4c4ba32f2b021f893
orifjonMAN <107062950+orifjonMAN@users.noreply.github.com>
2022-10-22 13:33:16 +0500

    after securing superliga

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://liganews.uz/']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
