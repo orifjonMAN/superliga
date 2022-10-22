fc38d5f86c586c5acbddba32dc2e3e362ab4a00c
orifjonMAN <107062950+orifjonMAN@users.noreply.github.com>
2022-10-22 17:26:17 +0500

    if debug

--- config/settings.py
+++ config/settings.py
@@ -165,8 +165,9 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
+    STATIC_ROOT = (BASE_DIR / '/staticfiles')
 else:
     STATIC_ROOT = (BASE_DIR / 'staticfiles')
 
