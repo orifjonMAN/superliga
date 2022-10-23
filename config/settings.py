2ab33a09781572c9655bdc37b604789841241847
orifjonMAN <107062950+orifjonMAN@users.noreply.github.com>
2022-10-23 10:32:05 +0500

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://liganews.uz/', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://liganews.uz/']
 
 
